b0cfb3f17b24173b3add97bea31d18dba50cbb58
Justin Willis <jgw9617@gmail.com>
2024-10-11 06:28:06 +0000

    add css parts to areas that make sense

--- src/templates/apple/template-apple.ts
+++ src/templates/apple/template-apple.ts
@@ -9,11 +9,11 @@ const template = (name: string, description: string, installDescription: string,
 
     return html`
         <aside id="pwa-install-element">
-            <article class="install-dialog apple ${classMap(installDialogClassesApple())} dialog-body">
+            <article class="install-dialog apple ${classMap(installDialogClassesApple())} dialog-body" part="install-dialog-apple">
                 <div class="icon">
                     <img src="${icon}" alt="icon" class="icon-image" draggable="false">
                 </div>
-                ${!disableClose? html`<button type="button" title="close" class="close" @click='${hideDialog}'>
+                ${!disableClose? html`<button type="button" title="close" class="close" part="close-button-apple" @click='${hideDialog}'>
                     <svg viewBox="0 0 24 24"><path d="M5.3 18.7c.2.2.4.3.7.3s.5-.1.7-.3l5.3-5.3 5.3 5.3a1.08 1.08 0 0 0 .7.3 1.08 1.08 0 0 0 .7-.3c.4-.4.4-1 0-1.4L13.4 12l5.3-5.3c.4-.4.4-1 0-1.4s-1-.4-1.4 0L12 10.6 6.7 5.3c-.4-.4-1-.4-1.4 0s-.4 1 0 1.4l5.3 5.3-5.3 5.3c-.4.4-.4 1 0 1.4z"/></svg>
                 </button>` : ''}
                 <div class="about">
